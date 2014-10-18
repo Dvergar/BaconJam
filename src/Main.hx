ad16a38dce58a4242d5f8f90d11fe4fc68a0a6d5
Dvergar <laurent.rd@gmail.com>
2014-10-18 10:10:31 +0200

    First commit - workflow related.

---
+++ src/Main.hx
@@ -0,0 +1,18 @@
+
+import luxe.Vector;
+import luxe.Input;
+import luxe.Text;
+import luxe.Color;
+import luxe.Rectangle;
+
+class Main extends luxe.Game {
+
+
+    override function ready() {
+
+        trace("hello bacon jam");
+
+    } //ready
+
+
+} //Main
