4847d90dcb49d61655e590f2ee4db1d4e40bcbe7
Dvergar <laurent.rd@gmail.com>
2014-10-18 11:32:17 +0200

    CONTROLS!

--- src/Main.hx
+++ src/Main.hx
@@ -34,8 +34,49 @@ class Main extends luxe.Game
     	player = new Player(Luxe.screen.mid.x, Luxe.screen.mid.y);
     }
 
+    var upPressed = false;
+    var downPressed = false;
+    var leftPressed = false;
+    var rightPressed = false;
+
+    override function onkeyup(e:KeyEvent)
+    {
+        if(e.keycode == Key.key_z)
+            upPressed = false;
+
+        if(e.keycode == Key.key_s)
+            downPressed = false;
+
+        if(e.keycode == Key.key_q)
+            leftPressed = false;
+
+        if(e.keycode == Key.key_d)
+            rightPressed = false;
+
+    }
+
+    override function onkeydown(e:KeyEvent)
+    {
+        if(e.keycode == Key.key_z)
+            upPressed = true;
+
+        if(e.keycode == Key.key_s)
+            downPressed = true;
+
+        if(e.keycode == Key.key_q)
+            leftPressed = true;
+
+        if(e.keycode == Key.key_d)
+            rightPressed = true;
+    }
+
+    static inline var SPEED:Int = 350;
+
     override function update(dt:Float)
     {
-    	player.sprite.pos.x += 1;
+    	if(upPressed) player.sprite.pos.y -= SPEED * dt;
+    	if(downPressed) player.sprite.pos.y += SPEED * dt;
+    	if(leftPressed) player.sprite.pos.x -= SPEED * dt;
+    	if(rightPressed) player.sprite.pos.x += SPEED * dt;
     }
 }
