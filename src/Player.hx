b5e3cf24d78bbbcb804335f49f0cefc49a2d0c3f
Dvergar <laurent.rd@gmail.com>
2014-10-19 01:49:52 +0200

    player animations, sprite placeholder

--- src/Player.hx
+++ src/Player.hx
@@ -1,4 +1,5 @@
 package;
+import luxe.components.sprite.SpriteAnimation;
 import components.BoxCollider;
 import components.ShootComponent;
 import luxe.Input.MouseButton;
@@ -15,6 +16,7 @@ class Player extends Sprite
 {
     static inline var SPEED:Int = 350;
 	var fireComponent:ShootComponent;
+	var anim:SpriteAnimation;
 
     public function new(x:Float, y:Float)
     {
@@ -25,9 +27,38 @@ class Player extends Sprite
             texture: texture,
             pos: new Vector(x, y),
             depth: 1,
-			name: "player"
-
+			name: "player",
+			size : new Vector(64, 64),
         });
+
+        texture.onload = function(t)
+        {
+	        anim = new SpriteAnimation({ name:'anim' });
+	        add(anim);
+
+	        var animation_json = '
+	            {
+	                "walk" : {
+	                    "frame_size":{ "x":"64", "y":"64" },
+	                    "frameset": ["1-3"],
+	                    "pingpong":"false",
+	                    "loop": "true",
+	                    "speed": "18"
+	                },
+	                "idle" : {
+	                    "frame_size":{ "x":"64", "y":"64" },
+	                    "frameset": ["1"],
+	                    "pingpong":"false",
+	                    "loop": "true",
+	                    "speed": "18"
+	                }
+	            }
+	        ';
+
+	        anim.add_from_json( animation_json );
+	        anim.animation = 'walk';
+	        anim.play();
+    	}
 
         Luxe.events.listen('update', update);
 		
@@ -41,7 +72,7 @@ class Player extends Sprite
 	
     override function update(dt:Float)
     {
-		move(dt);
+		move(dt);  // Why a function there? :(((
 		fireComponent.shooting = Luxe.input.mousedown(1);
 		fireComponent.direction = Vector.Subtract(LuxeApp._game.mousePos,pos).normalized;
     }
@@ -54,6 +85,14 @@ class Player extends Sprite
 			flipx = true;
 		if (input.x > 0)
 			flipx = false;
+
+		// ANIMATION EVENTS
+		if(anim == null) return;  // Meh
+		if(input.length == 0)
+			anim.animation = 'idle';
+		else
+			if(anim.animation == 'idle')
+				anim.animation = 'walk';
 	}
 	
 }
