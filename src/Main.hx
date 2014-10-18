0a10fcb0968cc96490e707dc3d403be820858bf0
Dvergar <laurent.rd@gmail.com>
2014-10-18 11:16:45 +0200

    Something on the screen!

--- src/Main.hx
+++ src/Main.hx
@@ -4,15 +4,38 @@ import luxe.Input;
 import luxe.Text;
 import luxe.Color;
 import luxe.Rectangle;
-
-class Main extends luxe.Game {
-
-
-    override function ready() {
-
-        trace("hello bacon jam");
-
-    } //ready
+import luxe.Sprite;
+import phoenix.Texture.FilterType;
 
 
-} //Main
+class Player
+{
+	public var sprite:Sprite;
+
+	public function new(x:Float, y:Float)
+	{
+        var texture = Luxe.loadTexture('assets/player.png');
+        texture.filter = FilterType.nearest;
+
+        sprite = new Sprite({
+            texture : texture,
+            pos : new Vector(x, y),
+        });
+	}
+}
+
+
+class Main extends luxe.Game
+{
+	var player:Player;
+
+    override function ready()
+    {
+    	player = new Player(Luxe.screen.mid.x, Luxe.screen.mid.y);
+    }
+
+    override function update(dt:Float)
+    {
+    	player.sprite.pos.x += 1;
+    }
+}
